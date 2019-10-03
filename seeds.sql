a3d1064eae9308518458a0217bc69f228c722c89
Mal <malrsteffes@gmail.com>
2019-10-03 10:49:20 -0400

    files updated to separate db structure from db seeds

--- seeds.sql
+++ seeds.sql
@@ -1,17 +1,4 @@
-DROP DATABASE IF EXISTS bamazonDB;
-
-CREATE DATABASE bamazonDB;
-
 USE bamazonDB;
-
-CREATE TABLE products (
-  item_id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(255) NOT NULL,
-  department_name VARCHAR(255) NOT NULL,
-  price INT default 0,
-  stock_quantity INT default 0,
-  PRIMARY KEY (item_id)
-);
 
 -- populate the database --
 
