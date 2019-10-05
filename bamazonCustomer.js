f88ea0ed16a06c3b3c9fa137992766aee1ad1648
Mal <malrsteffes@gmail.com>
2019-10-05 14:29:20 -0400

    customer app is now working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,17 +17,49 @@ var connection = mysql.createConnection({
 });
 
 //start function
-function start(){
-    console.log("Loading all products...");
-    connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-        // Log all results' ids, names and prices
-        for (i = 0; i < res.length; i++){
-            console.log(res[i].item_id + " | Product: " 
-            + res[i].product_name + " | $" + res[i].price);
-        }
-        connection.end();
-      });
+function start() {
+  console.log("Loading all products...");
+  connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
+    if (err) throw err;
+    // Log all results' ids, names and prices
+    console.table(res);
+
+
+    inquirer.prompt([{
+      name: "IDSearch",
+      type: "input",
+      message: "What is the ID of the product would you like to buy?",
+    },
+    {
+      name: "quantity",
+      type: "input",
+      message: "Please enter how many you would like to purchase."
+    }])
+      .then(function (answer) {
+        var idAnswer = answer.IDSearch;
+        var quantity = answer.quantity
+        connection.query("SELECT item_id, product_name, price, stock_quantity FROM products WHERE item_id =?",
+          [idAnswer], function (err, res) {
+            if (err) throw err;
+
+            // If the requested amount is less than what we have in stock
+            if (quantity < res[0].stock_quantity) {
+              var updatedStock = res[0].stock_quantity - quantity;
+              connection.query("UPDATE products SET stock_quantity=? WHERE item_id= ?", [updatedStock, idAnswer])
+                
+                console.log("Your purchase was successful! Your order total is $" + (quantity * res[0].price));
+            }
+            // If we don't have enough product to sell.
+            else {
+              console.log("Sorry! We are short-stocked on this item. We currently have " + res[0].stock_quantity + " available!");
+            }
+
+            connection.end();
+          })
+      })
+
+
+  });
 }
 
 start();
